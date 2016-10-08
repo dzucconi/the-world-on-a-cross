2f19d07f38e2db98a109c45568378cce06c2d6bb
dzucconi <mail@damonzucconi.com>
2016-10-08 12:16:17 +0200

    Balance the hypothetical die

--- app/javascripts/templates/cross.js
+++ app/javascripts/templates/cross.js
@@ -1,8 +1,8 @@
 export default dice => `
   <div class='dice'>
-    <div class='row'>${dice[0]}</div>
-    <div class='row'>${dice[1]}${dice[2]}${dice[3]}</div>
     <div class='row'>${dice[4]}</div>
+    <div class='row'>${dice[3]}${dice[0]}${dice[2]}</div>
+    <div class='row'>${dice[1]}</div>
     <div class='row'>${dice[5]}</div>
   </div>
 `;
